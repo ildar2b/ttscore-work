8c7e97b1bac8a0564b905ea9e81ba1cb88fce4b6
Ildar <53548114+ildar2b@users.noreply.github.com>
2019-09-02 14:05:28 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ var Player = function (lastName, firstName, ttwRating, nickName) {
 Player.prototype.confirmFirstServe = function () {
 // Чтобы не исчезала вся информация со страницы
 //	setTimeout(function() {
-	var	choice = confirm("Первым подает " + this.lastName + "?" + "\n[2.09.2019 14:59]");
+	var	choice = confirm("Первым подает " + this.lastName + "?" + "\n[2.09.2019 15:05]");
 //	}, 20);
 };
 
@@ -185,7 +185,7 @@ Player.prototype.showServe = function () {
 			speech.addEventListener("play", function() { // "playing" работает так же
 				setTimeout(function() {
 					speech.pause();
-				}, lengtnNum[num] + 300 );				
+				}, lengtnNum[num] + 400 );				
 			}, true);
 		return true;
 		};
@@ -194,12 +194,12 @@ Player.prototype.showServe = function () {
 			numSay(game.scoreA);
 			setTimeout(function() {
 				numSay(game.scoreB);
-			}, lengtnNum[game.scoreA] + 500 + say.pause);
+			}, lengtnNum[game.scoreA] + 600 + say.pause);
 		} else {
 			numSay(game.scoreB);
 			setTimeout(function() {
 				numSay(game.scoreA);
-			}, lengtnNum[game.scoreB] + 500 + say.pause);
+			}, lengtnNum[game.scoreB] + 600 + say.pause);
 		}
 	
 	};	
